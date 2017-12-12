d41030775190c9c1a510e09bc5a91895b66708b0
chiwawaweb <git@chiwawaweb.com>
2017-12-12 19:42:09 +0100

    Maj

---
+++ HifiBalises/MainForm.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace HifiBalises
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtBal1 = new System.Windows.Forms.TextBox();
+            this.TxtBal3 = new System.Windows.Forms.TextBox();
+            this.TxtBal2 = new System.Windows.Forms.TextBox();
+            this.TxtBal4 = new System.Windows.Forms.TextBox();
+            this.TxtBal8 = new System.Windows.Forms.TextBox();
+            this.TxtBal6 = new System.Windows.Forms.TextBox();
+            this.TxtBal7 = new System.Windows.Forms.TextBox();
+            this.TxtBal5 = new System.Windows.Forms.TextBox();
+            this.TxtBal12 = new System.Windows.Forms.TextBox();
+            this.TxtBal10 = new System.Windows.Forms.TextBox();
+            this.TxtBal11 = new System.Windows.Forms.TextBox();
+            this.TxtBal9 = new System.Windows.Forms.TextBox();
+            this.TxtBal14 = new System.Windows.Forms.TextBox();
+            this.TxtBal13 = new System.Windows.Forms.TextBox();
+            this.BtnPrint = new System.Windows.Forms.Button();
+            this.BtnQuit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtBal1
+            // 
+            this.TxtBal1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBal1.Location = new System.Drawing.Point(12, 12);
+            this.TxtBal1.Name = "TxtBal1";
+            this.TxtBal1.Size = new System.Drawing.Size(161, 29);
+            this.TxtBal1.TabIndex = 0;
+            // 
+            // TxtBal3
+            // 
+            this.TxtBal3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBal3.Location = new System.Drawing.Point(12, 47);
+            this.TxtBal3.Name = "TxtBal3";
+            this.TxtBal3.Size = new System.Drawing.Size(161, 29);
+            this.TxtBal3.TabIndex = 1;
+            // 
+            // TxtBal2
+            // 
+            this.TxtBal2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBal2.Location = new System.Drawing.Point(179, 12);
+            this.TxtBal2.Name = "TxtBal2";
+            this.TxtBal2.Size = new System.Drawing.Size(161, 29);
+            this.TxtBal2.TabIndex = 2;
+            // 
+            // TxtBal4
+            // 
+            this.TxtBal4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBal4.Location = new System.Drawing.Point(179, 47);
+            this.TxtBal4.Name = "TxtBal4";
+            this.TxtBal4.Size = new System.Drawing.Size(161, 29);
+            this.TxtBal4.TabIndex = 3;
+            // 
+            // TxtBal8
+            // 
+            this.TxtBal8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBal8.Location = new System.Drawing.Point(179, 117);
+            this.TxtBal8.Name = "TxtBal8";
+            this.TxtBal8.Size = new System.Drawing.Size(161, 29);
+            this.TxtBal8.TabIndex = 7;
+            // 
+            // TxtBal6
+            // 
+            this.TxtBal6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBal6.Location = new System.Drawing.Point(179, 82);
+            this.TxtBal6.Name = "TxtBal6";
+            this.TxtBal6.Size = new System.Drawing.Size(161, 29);
+            this.TxtBal6.TabIndex = 6;
+            // 
+            // TxtBal7
+            // 
+            this.TxtBal7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBal7.Location = new System.Drawing.Point(12, 117);
+            this.TxtBal7.Name = "TxtBal7";
+            this.TxtBal7.Size = new System.Drawing.Size(161, 29);
+            this.TxtBal7.TabIndex = 5;
+            // 
+            // TxtBal5
+            // 
+            this.TxtBal5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBal5.Location = new System.Drawing.Point(12, 82);
+            this.TxtBal5.Name = "TxtBal5";
+            this.TxtBal5.Size = new System.Drawing.Size(161, 29);
+            this.TxtBal5.TabIndex = 4;
+            // 
+            // TxtBal12
+            // 
+            this.TxtBal12.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBal12.Location = new System.Drawing.Point(179, 187);
+            this.TxtBal12.Name = "TxtBal12";
+            this.TxtBal12.Size = new System.Drawing.Size(161, 29);
+            this.TxtBal12.TabIndex = 11;
+            // 
+            // TxtBal10
+            // 
+            this.TxtBal10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBal10.Location = new System.Drawing.Point(179, 152);
+            this.TxtBal10.Name = "TxtBal10";
+            this.TxtBal10.Size = new System.Drawing.Size(161, 29);
+            this.TxtBal10.TabIndex = 10;
+            // 
+            // TxtBal11
+            // 
+            this.TxtBal11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBal11.Location = new System.Drawing.Point(12, 187);
+            this.TxtBal11.Name = "TxtBal11";
+            this.TxtBal11.Size = new System.Drawing.Size(161, 29);
+            this.TxtBal11.TabIndex = 9;
+            // 
+            // TxtBal9
+            // 
+            this.TxtBal9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBal9.Location = new System.Drawing.Point(12, 152);
+            this.TxtBal9.Name = "TxtBal9";
+            this.TxtBal9.Size = new System.Drawing.Size(161, 29);
+            this.TxtBal9.TabIndex = 8;
+            // 
+            // TxtBal14
+            // 
+            this.TxtBal14.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBal14.Location = new System.Drawing.Point(179, 222);
+            this.TxtBal14.Name = "TxtBal14";
+            this.TxtBal14.Size = new System.Drawing.Size(161, 29);
+            this.TxtBal14.TabIndex = 14;
+            // 
+            // TxtBal13
+            // 
+            this.TxtBal13.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBal13.Location = new System.Drawing.Point(12, 222);
+            this.TxtBal13.Name = "TxtBal13";
+            this.TxtBal13.Size = new System.Drawing.Size(161, 29);
+            this.TxtBal13.TabIndex = 12;
+            // 
+            // BtnPrint
+            // 
+            this.BtnPrint.Location = new System.Drawing.Point(179, 269);
+            this.BtnPrint.Name = "BtnPrint";
+            this.BtnPrint.Size = new System.Drawing.Size(75, 23);
+            this.BtnPrint.TabIndex = 15;
+            this.BtnPrint.Text = "Imprimer";
+            this.BtnPrint.UseVisualStyleBackColor = true;
+            // 
+            // BtnQuit
+            // 
+            this.BtnQuit.Location = new System.Drawing.Point(264, 269);
+            this.BtnQuit.Name = "BtnQuit";
+            this.BtnQuit.Size = new System.Drawing.Size(75, 23);
+            this.BtnQuit.TabIndex = 16;
+            this.BtnQuit.Text = "Quitter";
+            this.BtnQuit.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 304);
+            this.Controls.Add(this.BtnQuit);
+            this.Controls.Add(this.BtnPrint);
+            this.Controls.Add(this.TxtBal14);
+            this.Controls.Add(this.TxtBal13);
+            this.Controls.Add(this.TxtBal12);
+            this.Controls.Add(this.TxtBal10);
+            this.Controls.Add(this.TxtBal11);
+            this.Controls.Add(this.TxtBal9);
+            this.Controls.Add(this.TxtBal8);
+            this.Controls.Add(this.TxtBal6);
+            this.Controls.Add(this.TxtBal7);
+            this.Controls.Add(this.TxtBal5);
+            this.Controls.Add(this.TxtBal4);
+            this.Controls.Add(this.TxtBal2);
+            this.Controls.Add(this.TxtBal3);
+            this.Controls.Add(this.TxtBal1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Hifi Balises";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtBal1;
+        private System.Windows.Forms.TextBox TxtBal3;
+        private System.Windows.Forms.TextBox TxtBal2;
+        private System.Windows.Forms.TextBox TxtBal4;
+        private System.Windows.Forms.TextBox TxtBal8;
+        private System.Windows.Forms.TextBox TxtBal6;
+        private System.Windows.Forms.TextBox TxtBal7;
+        private System.Windows.Forms.TextBox TxtBal5;
+        private System.Windows.Forms.TextBox TxtBal12;
+        private System.Windows.Forms.TextBox TxtBal10;
+        private System.Windows.Forms.TextBox TxtBal11;
+        private System.Windows.Forms.TextBox TxtBal9;
+        private System.Windows.Forms.TextBox TxtBal14;
+        private System.Windows.Forms.TextBox TxtBal13;
+        private System.Windows.Forms.Button BtnPrint;
+        private System.Windows.Forms.Button BtnQuit;
+    }
+}
+
